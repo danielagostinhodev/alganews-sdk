9056e1e2cae3230a28b70bad57e006cd65a6d489
Daniel <daniel.agostinho@hotmail.com>
2022-11-06 16:57:49 -0300

    feat: add base service

---
+++ src/services/Metric.service.ts
@@ -0,0 +1,18 @@
+import Service from "../Service";
+import {Metric} from "../@types";
+
+class MetricService extends Service {
+    static getTop3Tags(){
+        return this.Http
+            .get<Metric.EditorTagRatio>('/metrics/editor/top3-tags')
+            .then(this.getData)
+    }
+
+    static getEditorMonthlyEarnings() {
+        return this.Http
+            .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
+            .then(this.getData)
+    }
+}
+
+export default MetricService
