68a11c303f1be6948151c484eabcaeddba6523cd
Daniel <daniel.agostinho@hotmail.com>
2022-11-14 10:29:31 -0300

    feat: 9.13. Content-Type personalizado

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -13,6 +13,26 @@ class MetricService extends Service {
             .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
             .then(this.getData)
     }
+
+    static getMonthlyReveneusExpenses(){
+        const headers = {
+            'Content-Type' : 'application/json'
+        }
+
+        return this.Http
+            .get<Metric.MonthlyRevenuesExpenses>('/metrics/monthly-reveneus-expenses', {headers})
+            .then(this.getData)
+    }
+
+    static getMonthlyReveneusExpensesChartJs(){
+        const headers = {
+            'Content-Type' : 'application/vnd.alganews.chartjs+json'
+        }
+
+        return this.Http
+            .get<Metric.MonthlyRevenuesExpenses>('/metrics/monthly-reveneus-expenses', {headers})
+            .then(this.getData)
+    }
 }
 
 export default MetricService
